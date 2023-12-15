e301c1639745bc8c0f54a26b6c4885f819bc8241
Ibrahim Javed <ibrahim.javed@A006-01369.local>
2023-12-15 18:57:44 +0500

    topic to subject mapping

--- create_csv.py
+++ create_csv.py
@@ -16,14 +16,6 @@ def load_api_data_from_json(filename='api_data.json'):
         print(f"{filename} not found.")
         return []
 
-# def create_ocw_topic_to_oer_subject_mapping(filename='mapping_files/ocw_topic_to_oer_subject.csv'):
-#     ocw_topic_to_subject = pd.read_csv(filename)
-#     ocw_topics_mapping = ocw_topic_to_subject['OER Subject'].to_dict()
-#     missing_subjects = ocw_topic_to_subject[ocw_topic_to_subject['OER Subject'].isna()]
-#     ocw_topics_mapping.update(missing_subjects.set_index('OCW Topic')['OER Keyword'].to_dict())
-#     print(ocw_topics_mapping)
-#     return ocw_topics_mapping
-
 def create_ocw_topic_to_oer_subject_mapping(filename='mapping_files/ocw_topic_to_oer_subject.csv'):
     ocw_topic_to_subject = pd.read_csv(filename)
     # ocw_topics_mapping = dict(zip(ocw_topic_to_subject['OCW Topic'], ocw_topic_to_subject['OER Subject']))
@@ -35,7 +27,8 @@ def create_ocw_topic_to_oer_subject_mapping(filename='mapping_files/ocw_topic_to
 
 def get_oer_subjects_for_ocw_course(ocw_topics_mapping, ocw_course_topics):
     # Returns pipe-separated string of OER subjects for an OCW course
-    oer_subjects_list = [ocw_topics_mapping.get(topic['name']).split('|') for topic in ocw_course_topics]
+    oer_subjects_list = [ocw_topics_mapping.get(topic['name']).split('|') if ocw_topics_mapping.get(topic['name']) is not None else [] for topic in ocw_course_topics]
+
     unique_oer_subjects = set(subject for subjects in oer_subjects_list for subject in subjects)
     return '|'.join(unique_oer_subjects)
 
@@ -48,14 +41,12 @@ def process_data(data):
 def main():
     api_url = getenv('API_URL')
     api_data_json = load_api_data_from_json()
-    ocw_topics_mapping = create_ocw_topic_to_oer_subject_mapping()
-    get_oer_subjects_for_ocw_course(ocw_topics_mapping,[{'id': 124, 'name': 'Environmental Engineering'}, {'id': 22, 'name': 'Engineering'}, {'id': 199, 'name': 'Civil Engineering'}])
-    # processed_data = process_data(api_data_json)
-    # final_df = pd.DataFrame(processed_data, columns=["CR_TITLE", "CR_URL", "CR_LANGUAGE", "CR_COU_TITLE", "CR_PRIMARY_USER", "CR_SUBJECT"])
+    processed_data = process_data(api_data_json)
+    final_df = pd.DataFrame(processed_data, columns=["CR_TITLE", "CR_URL", "CR_LANGUAGE", "CR_COU_TITLE", "CR_PRIMARY_USER", "CR_SUBJECT"])
 
-    # print(final_df)
+    print(final_df)
 
-    # final_df.to_csv('transformed_data.csv', index=False)
+    final_df.to_csv('transformed_data.csv', index=False)
 
 if __name__ == "__main__":
     main()
