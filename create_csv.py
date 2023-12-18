2f61c8f8ee667661ce5dacedeb7269a000d2bb2f
Ibrahim Javed <ibrahim.javed@A006-01369.local>
2023-12-18 13:30:51 +0500

    add markdown stripper

--- create_csv.py
+++ create_csv.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from os import getenv
 
 from client import fetch_all_data_from_api
+from utilities.markdown_to_text import markdown_to_text
+from utilities.markdown_to_text2 import unmark
 load_dotenv()
 
 def load_api_data_from_json(filename='api_data.json'):
@@ -18,31 +20,32 @@ def load_api_data_from_json(filename='api_data.json'):
 
 def create_ocw_topic_to_oer_subject_mapping(filename='mapping_files/ocw_topic_to_oer_subject.csv'):
     ocw_topic_to_subject = pd.read_csv(filename)
-    # ocw_topics_mapping = dict(zip(ocw_topic_to_subject['OCW Topic'], ocw_topic_to_subject['OER Subject']))
     ocw_topics_mapping = ocw_topic_to_subject.set_index('OCW Topic')['OER Subject'].to_dict()
-    missing_subjects = ocw_topic_to_subject[ocw_topic_to_subject['OER Subject'].isna()]
-    # ocw_topics_mapping.update(dict(zip(missing_subjects['OCW Topic'], missing_subjects['OER Keyword'])))
-    ocw_topics_mapping.update(missing_subjects.set_index('OCW Topic')['OER Keyword'].to_dict())
     return ocw_topics_mapping
 
 def get_oer_subjects_for_ocw_course(ocw_topics_mapping, ocw_course_topics):
     # Returns pipe-separated string of OER subjects for an OCW course
     oer_subjects_list = [ocw_topics_mapping.get(topic['name']).split('|') if ocw_topics_mapping.get(topic['name']) is not None else [] for topic in ocw_course_topics]
-
     unique_oer_subjects = set(subject for subjects in oer_subjects_list for subject in subjects)
     return '|'.join(unique_oer_subjects)
+
+def get_description_in_plain_text(description):
+    return unmark(description)
+
+def get_description_in_plain_text2(description):
+    return markdown_to_text(description)
 
 def process_data(data):
     ocw_topics_mapping = create_ocw_topic_to_oer_subject_mapping()
 
-    processed_data = [{"CR_TITLE": result["title"], "CR_URL": result["runs"][0]["url"], "CR_LANGUAGE": "en", "CR_COU_TITLE": "Creative Commons Attribution Non Commercial Share Alike 4.0", "CR_PRIMARY_USER": "student|teacher", "CR_SUBJECT": get_oer_subjects_for_ocw_course(ocw_topics_mapping, result["topics"])} for result in data if result.get("runs")]
+    processed_data = [{"CR_TITLE": result["title"], "CR_URL": result["runs"][0]["url"], "CR_MATERIAL_TYPE": "Full Course", "CR_Media_Formats": "Text/HTML", "CR_ABSTRACT": get_description_in_plain_text(result["runs"][0]["description"]), "CR_LANGUAGE": "en", "CR_COU_TITLE": "Creative Commons Attribution Non Commercial Share Alike 4.0", "CR_PRIMARY_USER": "student|teacher", "CR_SUBJECT": get_oer_subjects_for_ocw_course(ocw_topics_mapping, result["topics"])} for result in data if result.get("runs")]
     return processed_data
 
 def main():
     api_url = getenv('API_URL')
     api_data_json = load_api_data_from_json()
     processed_data = process_data(api_data_json)
-    final_df = pd.DataFrame(processed_data, columns=["CR_TITLE", "CR_URL", "CR_LANGUAGE", "CR_COU_TITLE", "CR_PRIMARY_USER", "CR_SUBJECT"])
+    final_df = pd.DataFrame(processed_data, columns=["CR_TITLE", "CR_URL", "CR_MATERIAL_TYPE", "CR_Media_Formats", "CR_ABSTRACT", "CR_LANGUAGE", "CR_COU_TITLE", "CR_PRIMARY_USER", "CR_SUBJECT"])
 
     print(final_df)
 
---
+++ utilities/html_to_text.py
@@ -0,0 +1,6 @@
+# import re
+
+# pattern = re.compile('<.*?>')
+
+
+# result = re.sub(pattern, '', html_string)
