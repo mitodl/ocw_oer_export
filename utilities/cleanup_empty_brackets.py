efaae271e4c0e31e5c496cb0a391931df4b0909a
Ibrahim Javed <ibrahim.javed@A006-01369.local>
2023-12-19 14:13:00 +0500

    modify file structure

--- utilities/cleanup_empty_brackets.py
+++
@@ -1,4 +0,0 @@
-import re
-def cleanup_empty_brackets(string):
-    pattern = re.compile('{{}}')
-    return re.sub(pattern, '', string)
--- utilities/html_to_text.py
+++
@@ -1,4 +0,0 @@
-import re
-def html_to_text(html):
-    pattern = re.compile('<.*?>')
-    return re.sub(pattern, '', html)
