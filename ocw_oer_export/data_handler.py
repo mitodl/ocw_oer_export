480150ac7ebec219079d9eb51f2f358b731eb28d
ibrahimjaved12 <ibrahim.javed@arbisoft.com>
2024-01-15 17:49:57 +0500

    Update file path

--- ocw_oer_export/data_handler.py
+++ ocw_oer_export/data_handler.py
@@ -3,13 +3,10 @@ Module for extracting and loading data to/from JSON file
 """
 import json
 import logging
-import os
 
 
-def extract_data_from_json(file_name):
+def extract_data_from_json(file_path):
     """Extract data from a JSON file."""
-    file_dir = "/output"  # Set the output directory inside the container
-    file_path = os.path.join(file_dir, file_name)
     try:
         with open(file_path, "r", encoding="utf-8") as json_file:
             data = json.load(json_file)
