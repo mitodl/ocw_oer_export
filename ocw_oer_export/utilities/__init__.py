059b141c6296152fff3452f428769d20e044b9f0
ibrahimjaved12 <ibrahim.javed@arbisoft.com>
2024-01-19 18:51:19 +0500

    Use FM exported keywords

--- ocw_oer_export/utilities/__init__.py
+++ ocw_oer_export/utilities/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ["text_cleanup", "delete_file"]
+__all__ = ["normalize_course_url", "text_cleanup", "delete_file"]
 
+from .normalize_course_url import normalize_course_url
 from .text_cleanup import text_cleanup
 from .delete_file import delete_file
