575d25776a629b5cb070b4af61546359091b2470
ibrahimjaved12 <ibrahim.javed@arbisoft.com>
2024-01-22 14:55:02 +0500

    added another input example for normalize keywords

--- ocw_oer_export/utilities/normalize_keywords.py
+++ ocw_oer_export/utilities/normalize_keywords.py
@@ -10,13 +10,14 @@ def normalize_keywords(keywords):
     Normalizes keywords from the OCW FM export file to a standardized, pipe-separated format.
 
     The OCW FM export file may contain keywords in various formats, including comma-separated,
-    semicolon-separated, newline-separated, double newlines separated, or combinations of these.
+    semicolon-separated, newline-separated, double newlines separated, or multiples of these.
 
     This function converts these formats into a standardized format used in OER: a pipe-separated
     string where each keyword's first letter is capitalized.
 
     Example:
-    Input:  "novel, short story; the city in literature\nnarrative voice"
+    Input:  "novel, short story, the city in literature,narrative voice"
+    Input:  "novel\n\nshort story\n\nthe city in literature\n\nnarrative voice"
     Output: "Novel|Short Story|The City In Literature|Narrative Voice"
     """
     normalized_keywords = re.sub(r"[;,]|\n\n|\n", "|", keywords).strip()
